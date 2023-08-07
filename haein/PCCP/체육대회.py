44d106ffdb6bc7a33bf0e5178fce8aa1de9c0d7b
haein-side <kimhi1640@gmail.com>
2023-08-07 22:32:56 +0900

    pccp

--- haein/PCCP/체육대회.py
+++ haein/PCCP/체육대회.py
@@ -1,13 +1,13 @@
 from itertools import permutations
 
 def solution(ability):
-    answer = 0
+    result = 0
     arr = list(permutations(ability, len(ability[0])))
 
     for i in range(len(arr)):
         total = 0
         for j in range(len(ability[0])):
             total += arr[i][j][j]
-        answer = max(answer, total)
+        result = max(result, total)
 
-    return answer
+    return result
